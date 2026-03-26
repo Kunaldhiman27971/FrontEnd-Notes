affe539bebea3f09112588f42b3480ab659d8326
Kunal <kunaldhiman27971@gmail.com>
2026-03-27 01:48:22 +0530

    two way binding under process

---
+++ twoWayBinding/Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' text-xl font-bold text-center bg-emerald-300 p-5 rounded-lg h-screen w-screen'>
+      App
+    </div>
+  )
+}
+
+export default App
