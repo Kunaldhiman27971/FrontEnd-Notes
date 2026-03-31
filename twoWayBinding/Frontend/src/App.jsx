9f21d4cb517a99eef90a16b8c271659103927ff6
Kunal <kunaldhiman27971@gmail.com>
2026-03-31 16:59:57 +0530

    two way binding done

--- twoWayBinding/Frontend/src/App.jsx
+++ twoWayBinding/Frontend/src/App.jsx
@@ -1,11 +1,51 @@
+// Form Handling in React
+
 import React from 'react'
+import { useState } from 'react'
+
 
 const App = () => {
+
+  const [username, setusername] = useState("")
+
+  const [allusers, setallusers] = useState([''])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setallusers([...allusers, username])
+    setusername("")
+  }
+
+
   return (
-    <div className=' text-xl font-bold text-center bg-emerald-300 p-5 rounded-lg h-screen w-screen'>
-      App
+    <div>
+      <form onSubmit={(e)=>{
+        handleSubmit(e)
+      }}
+       className='p-5 flex flex-col gap-5 w-fit'>
+
+
+        <input 
+        className="bg-black rounded-2xl p-2 text-white w-fit" 
+        type="text " placeholder='Enter Name'
+        value={username}
+        required={true}
+        onChange={(e)=>{
+          setusername(e.target.value)
+        }}/>
+
+
+        <button className='bg-black rounded-2xl p-2 text-white w-fit '>Submit</button>
+      </form>
+
+      <div>
+        {allusers.map((elem,index)=>{
+          return <h1 key={index}>{elem}</h1>
+        })}
+      </div>
     </div>
   )
 }
 
 export default App
+
